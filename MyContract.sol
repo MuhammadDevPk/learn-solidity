ab99a57ccbb048db1fa01014c7604cfb0eb00eb3
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-01-03 18:17:00 +0500

    learned functions

---
+++ MyContract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.17;
+
+contract MyContract {
+    string name = "Example 1";
+
+    function setName(string memory _name) public {
+        name = _name;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function resetName() internal {
+        name = "Example 1";
+    }
+}
