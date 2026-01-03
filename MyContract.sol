f6395aedb4c4c0d0c692784e38938c1f410065a5
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-01-03 21:40:11 +0500

    learned constructor

--- MyContract.sol
+++ MyContract.sol
@@ -3,15 +3,9 @@
 pragma solidity ^0.8.17;
 
 contract MyContract {
-    address private owner;
-    string public name = "";
+    string public name;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
-    }
-
-    function setName(string memory _name) public onlyOwner {
+    constructor(string memory _name) {
         name = _name;
     }
 }
