271891aaa45354b7de23b7ef32a727d4b63cf849
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-01-04 17:04:22 +0500

    learned events

--- MyContract.sol
+++ MyContract.sol
@@ -3,26 +3,12 @@
 pragma solidity ^0.8.17;
 
 contract MyContract {
-    struct Book {
-        string title;
-        string author;
-        bool completed;
-    }
+    string public message = "Hello World";
 
-    Book[] public books;
+    event MessageUpdate(address indexed _user, string _message)
 
-    function add(string memory _title, string memory _author) public {
-        books.push(Book(_title, _author, false));
-    }
-
-    function get(
-        uint _index
-    )
-        public
-        view
-        returns (string memory title, string memory author, bool completed)
-    {
-        Book storage book = books[_index];
-        return (book.title, book.author, book.completed);
+    function updateMessage(string memory _message) public {
+        message = _message;
+        emit MessageUpdate(msg.sender, _message);
     }
 }
