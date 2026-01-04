22d3223f0c2d632661583356a3cd4e839c10e491
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-01-04 19:59:35 +0500

    learned Ether

--- MyContract.sol
+++ MyContract.sol
@@ -3,12 +3,27 @@
 pragma solidity ^0.8.17;
 
 contract MyContract {
-    string public message = "Hello World";
+    uint public value1 = 1 wei;
+    uint public value2 = 1;
+    uint public value3 = 1 gwei;
+    uint public value4 = 1000000000;
+    uint public value5 = 1 ether;
+    uint public value6 = 1000000000000000000;
 
-    event MessageUpdate(address indexed _user, string _message)
+    uint public count = 0;
 
-    function updateMessage(string memory _message) public {
-        message = _message;
-        emit MessageUpdate(msg.sender, _message);
+    receive() external payable {}
+
+    fallback() external payable {
+        count++;
+    }
+
+    function checkBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transfer(address payable _to) public payable {
+        (bool, sent) = _to.call{value: msg.value}("");
+        require(sent, "Failed!");
     }
 }
